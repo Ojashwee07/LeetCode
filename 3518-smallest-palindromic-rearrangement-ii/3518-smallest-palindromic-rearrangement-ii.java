9da6b379cbea946b2b83dd97b17a4dc1710cf97b
Ojashwee <166322643+Ojashwee07@users.noreply.github.com>
2026-07-29 13:39:44 +0530

    Time: 16 ms (90.32%), Space: 47.1 MB (74.19%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -1,104 +1,76 @@
-import java.math.BigInteger;
-
 class Solution {
 
-    static final int LIMIT = 1_000_000;
+    private long comb(long n, long m, long k) {
+        long res = 1;
+        m = Math.min(m, n - m);
 
-    public String smallestPalindrome(String s, int k) {
+        for (long i = 1; i <= m; i++) {
+            res = (res * (n - i + 1)) / i;
+            if (res > k) {
+                return k + 1;
+            }
+        }
+        return res;
+    }
 
-        int[] freq = new int[26];
-
-        for (char c : s.toCharArray())
-            freq[c - 'a']++;
-
-        String mid = "";
-        int[] half = new int[26];
-        int halfLen = 0;
-
+    private long permutations(int rem, int[] bucket, long k) {
+        long ways = 1;
         for (int i = 0; i < 26; i++) {
-            if ((freq[i] & 1) == 1)
-                mid = String.valueOf((char) ('a' + i));
+            if (bucket[i] == 0) {
+                continue;
+            }
 
-            half[i] = freq[i] / 2;
-            halfLen += half[i];
+            ways *= comb(rem, bucket[i], k);
+            if (ways > k) {
+                break;
+            }
+            rem -= bucket[i];
+        }
+        return ways;
+    }
+
+    public String smallestPalindrome(String s, long k) {
+        int partition = s.length() / 2;
+        int[] bucket = new int[26];
+
+        for (int i = 0; i < partition; i++) {
+            bucket[s.charAt(i) - 97] += 1;
         }
 
         StringBuilder left = new StringBuilder();
+        long startIndex = 1;
 
-        for (int pos = 0; pos < halfLen; pos++) {
-
-            for (int c = 0; c < 26; c++) {
-
-                if (half[c] == 0)
+        for (int pos = 0; pos < partition; pos++) {
+            for (int i = 0; i < 26; i++) {
+                if (bucket[i] == 0) {
                     continue;
+                }
 
-                half[c]--;
+                bucket[i] -= 1;
 
-                int ways = countWays(half);
-
-                if (ways >= k) {
-                    left.append((char) ('a' + c));
+                long ways = permutations(partition - pos - 1, bucket, k);
+                if (startIndex + ways > k) {
+                    left.append((char) (i + 97));
                     break;
                 }
 
-                k -= ways;
-                half[c]++;
+                bucket[i] += 1;
+                startIndex += ways;
             }
         }
 
-        StringBuilder ans = new StringBuilder(left);
-        ans.append(mid);
-
-        for (int i = left.length() - 1; i >= 0; i--)
-            ans.append(left.charAt(i));
-
-        if (k > 1) return "";
-
-        return ans.toString();
-    }
-
-    private int countWays(int[] cnt) {
-
-        int total = 0;
-        for (int x : cnt) total += x;
-
-        BigInteger res = BigInteger.ONE;
-
-        int remain = total;
-
-        for (int x : cnt) {
-
-            if (x == 0) continue;
-
-            res = res.multiply(binomial(remain, x));
-
-            if (res.compareTo(BigInteger.valueOf(LIMIT)) > 0)
-                return LIMIT + 1;
-
-            remain -= x;
+        if (left.length() < partition) {
+            return "";
         }
 
-        if (res.compareTo(BigInteger.valueOf(LIMIT)) > 0)
-            return LIMIT + 1;
-
-        return res.intValue();
-    }
-
-    private BigInteger binomial(int n, int r) {
-
-        if (r > n - r)
-            r = n - r;
-
-        BigInteger ans = BigInteger.ONE;
-
-        for (int i = 1; i <= r; i++) {
-            ans = ans.multiply(BigInteger.valueOf(n - r + i));
-            ans = ans.divide(BigInteger.valueOf(i));
-
-            if (ans.compareTo(BigInteger.valueOf(LIMIT)) > 0)
-                return BigInteger.valueOf(LIMIT + 1);
+        if (s.length() % 2 != 0) {
+            left.append(s.charAt(partition));
         }
 
-        return ans;
+        for (int i = partition - 1; i >= 0; i--) {
+            left.append(left.charAt(i));
+        }
+
+        return left.toString();
     }
 }
