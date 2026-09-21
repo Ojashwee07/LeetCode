a61af0e760752cdcd03b19e053e07793067d75f0
Ojashwee <166322643+Ojashwee07@users.noreply.github.com>
2026-09-21 11:01:28 +0530

    Time: 12 ms (92.31%), Space: 91.8 MB (96.15%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] ans = new long[k];
+        long[] dp = new long[k];
+
+        for (int num : nums) {
+            long[] next = new long[k];
+
+            // Start a new subarray with current element
+            int rem = num % k;
+            next[rem]++;
+
+            // Extend previous subarrays
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    int newRem = (r * rem) % k;
+                    next[newRem] += dp[r];
+                }
+            }
+
+            // Add subarrays ending at current index
+            for (int r = 0; r < k; r++) {
+                ans[r] += next[r];
+            }
+
+            dp = next;
+        }
+
+        return ans;
+    }
+}
